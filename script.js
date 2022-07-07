011840db02810039f23892ace44586b06b76d9b4
ibrammalik <malikmaul1173@gmail.com>
2022-07-07 10:37:53 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const sideButton = document.getElementById("menu-label");
+const sidebar = document.getElementsByClassName("sidebar")[0];
+const arrow = document.getElementsByClassName("arrow")[0];
+const arrow1 = document.getElementsByClassName("arrow1")[0];
+
+sideButton.addEventListener("click", function () {
+  sidebar.classList.toggle("show");
+  arrow.classList.toggle("rotate");
+  arrow1.classList.toggle("rotate1");
+  console.log("ok");
+});
+
+//mobile nav menu
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector(".nav-kanan");
+
+menuToggle.addEventListener("click", function () {
+  menuToggle.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  sidebar.classList.toggle("active");
+});
+
+//close mobile nav menu when clicked any menu
+const clickedMenu = document.querySelectorAll(".nav-link");
+
+clickedMenu.forEach((n) =>
+  n.addEventListener("click", function () {
+    menuToggle.classList.remove("active");
+    navMenu.classList.remove("active");
+    sidebar.classList.remove("active");
+  })
+);
